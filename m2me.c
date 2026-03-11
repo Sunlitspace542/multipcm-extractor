c4c40ce4ef19b88a12cddef18276037cf818392d
SunlitSpace542 <35207254+Sunlitspace542@users.noreply.github.com>
2026-03-10 21:21:47 -0400

    better error message for drag-and-drop mode

--- m2me.c
+++ m2me.c
@@ -223,7 +223,7 @@ int main(int argc, char *argv[]) {
     FILE *mpr;
     mpr = fopen(infile, "rb");
     if (mpr == NULL) {
-        printf("Input file does not exist or invalid argument(s) specified\n");
+        printf("Bad command or filename\nRun m2me -help for help.\n");
         return 1;
     }
     
